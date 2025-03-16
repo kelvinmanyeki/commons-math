79862682216c18f8ddaa971075f710cc4b10075f
kelvinmanyeki <kelvinmanyeki@gmail.com>
2025-03-16 19:30:52 +0100

    fixed javadoc checkstyle error

--- predictive-maintenance/src/main/java/com/myproject/Main.java
+++ predictive-maintenance/src/main/java/com/myproject/Main.java
@@ -1,10 +1,10 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
+ * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * the License.  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -95,7 +95,7 @@ public final class Main {
      */
     public static double calculateBaseFailureProbability(final double[] features) {
         if (features == null || features.length != 3) {
-            throw new IllegalArgumentException("Input array must have exactly 3 elements.");
+            throw new IllegalArgumentException("Input array must have exactly 3 elements");
         }
         return (features[0] * 0.0004) + (features[1] * 0.002) + (features[2] * 0.0015);
     }
